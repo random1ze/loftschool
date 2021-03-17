e022d1c1682b4cf3f0fd35bd004e776e06bd68ee
Sergei Abrasov <intel7@yandex.ru>
2021-03-17 15:32:34 +0300

    feat: done sidebar with script and add animation stiles

---
+++ lost-shirt/scripts/sidebar-menu.js
@@ -0,0 +1,13 @@
+const openingButton = document.querySelector('.sidebar__hamburger');
+const closingButton = document.querySelector('.sidebar__close');
+const sidebar = document.querySelector('.sidebar');
+
+function openSidebar() {
+	sidebar.classList.add('sidebar--opened');
+}
+function closeSidebar() {
+	sidebar.classList.remove('sidebar--opened');
+}
+
+openingButton.addEventListener("click", openSidebar);
+closingButton.addEventListener("click", closeSidebar);
