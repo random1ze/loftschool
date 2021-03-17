edeadfaa6c23d1c6bacae08fe642e28f07938f69
Sergei Abrasov <intel7@yandex.ru>
2021-03-17 16:30:31 +0300

    refactor: add script isolate with IIFE

--- lost-shirt/scripts/sidebar-menu.js
+++ lost-shirt/scripts/sidebar-menu.js
@@ -1,13 +1,15 @@
-const openingButton = document.querySelector('.sidebar__hamburger');
-const closingButton = document.querySelector('.sidebar__close');
-const sidebar = document.querySelector('.sidebar');
+(function () {
+  const openingButton = document.querySelector(".sidebar__hamburger");
+  const closingButton = document.querySelector(".sidebar__close");
+  const sidebar = document.querySelector(".sidebar");
 
-function openSidebar() {
-	sidebar.classList.add('sidebar--opened');
-}
-function closeSidebar() {
-	sidebar.classList.remove('sidebar--opened');
-}
+  function openSidebar() {
+    sidebar.classList.add("sidebar--opened");
+  }
+  function closeSidebar() {
+    sidebar.classList.remove("sidebar--opened");
+  }
 
-openingButton.addEventListener("click", openSidebar);
-closingButton.addEventListener("click", closeSidebar);
+  openingButton.addEventListener("click", openSidebar);
+  closingButton.addEventListener("click", closeSidebar);
+})();
